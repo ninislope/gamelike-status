c2572eea6b643c31d37f394e829fa7181c096b0e
ninislope <ninislope@narazaka.net>
2019-02-19 12:04:28 +0900

    mv

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
