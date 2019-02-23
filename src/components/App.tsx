2cfdd57a021088ecd9bd4c3134ff89b28a4ed0d1
ninislope <ninislope@narazaka.net>
2019-02-23 23:59:39 +0900

    キャンセルボタン

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,7 +16,10 @@ export const App = observer<React.SFC<{store: Store}>>(({store}) =>
                     store.loginUid ? (
                         store.loginUid === store.dataUid && (
                             store.ui.editable ?
-                            <button onClick={() => store.trySave()}>保存</button> :
+                            <>
+                                <button onClick={() => store.trySave()}>保存</button>
+                                <button onClick={() => { if (confirm("変更した内容が失われます。よろしいですか？")) location.href = location.href; }}>キャンセル</button>
+                            </> :
                             <button onClick={() => store.tryEdit()}>編集</button>
                         )
                     ) :
