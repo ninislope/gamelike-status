30db5617a6637782ab2dae2a40a2c958834beb44
ninislope <ninislope@narazaka.net>
2020-07-30 00:21:50 +0900

    white color

--- src/components/StatusView.tsx
+++ src/components/StatusView.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { observer } from "mobx-react";
+import classNames from "classnames";
 import { Character } from "../Store/Character";
 import * as style from "./StatusView.module.css";
 import { PageSelectorsView } from "./PageSelectorsView";
@@ -9,11 +10,11 @@ import { injectSafe } from "../injectSafe";
 
 export const StatusView = injectSafe("ui")(observer<React.SFC<{character: Character; ui: UI}>>(({character, ui}) => {
     if (!character.periods.length) {
-        return <div className={style.wrapper}><div>時期を追加して下さい</div></div>;
+        return <div className={classNames(style.wrapper, style.addMessage)}><div>時期を追加して下さい</div></div>;
     }
     const period = character.period(ui.period);
     if (!period) {
-        return <div className={style.wrapper}><div>時期を選択して下さい</div></div>;
+        return <div className={classNames(style.wrapper, style.addMessage)}><div>時期を選択して下さい</div></div>;
     }
     const previousPeriod = character.period(ui.period - 1);
     const page = period && period.pages.length && period.page(ui.page);
@@ -23,7 +24,7 @@ export const StatusView = injectSafe("ui")(observer<React.SFC<{character: Charac
             {
                 !!page ?
                 <PageView character={character} period={period} page={page} /> :
-                <div>ページを追加して下さい</div>
+                <div className={style.addMessage}>ページを追加して下さい</div>
             }
         </div>
         <div key="PageSelectorsView">
