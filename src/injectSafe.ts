d00af7a732172dac52c08dbf7442f2e9e61161f5
ninislope <ninislope@narazaka.net>
2020-07-30 02:24:01 +0900

    injectSafe multi key

--- src/injectSafe.ts
+++ src/injectSafe.ts
@@ -3,8 +3,8 @@ import { ComponentType } from "react";
 
 export type DiffKey<T, U> = T extends U ? never : T;
 
-export function injectSafe<K extends string>(key: K) {
+export function injectSafe<K extends string>(...keys: K[]) {
     return function <T>(component: ComponentType<T>) {
-        return inject(key)(component) as unknown as ComponentType<Pick<T, DiffKey<keyof T, K>>>;
+        return inject(...keys)(component) as unknown as ComponentType<Pick<T, DiffKey<keyof T, K>>>;
     };
 }
